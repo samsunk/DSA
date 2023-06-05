212333be4819a11d76a44ce365b714bf85c63b76
samsunk <hamdard.samsunk@gmail.com>
2023-06-05 14:06:31 +0545

    correction

--- stack.c
+++ stack.c
@@ -1,56 +1,66 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define MAX 10
 
-int count=0;
+int count = 0;
 // defining a stack template
-struct stack{
+struct stack
+{
     int items[MAX];
     int top;
 };
 typedef struct stack mystack;
 
 // creating empty stack
-void createEmptyStack(mystack *s){
-s->top=-1;
+void createEmptyStack(mystack *s)
+{
+    s->top = -1;
 }
 
 // check if the stack is full
-int isFull(mystack *s){
-    if(s->top==MAX-1)
-    return 1;
+int isFull(mystack *s)
+{
+    if (s->top == MAX - 1)
+        return 1;
     else
-    return 0;
+        return 0;
 }
 
 // check if the stack is empty
-int isEmpty(mystack *s){
-    if(s->top==-1)
-    return 1;
+int isEmpty(mystack *s)
+{
+    if (s->top == -1)
+        return 1;
     else
-    return 0;
+        return 0;
 }
 
 // Add elements into stack
-void push(mystack *s,int newitem){
-    if(isFull){
+void push(mystack *s, int newitem)
+{
+    if (isFull(s))
+    {
         printf("Stack is full\n");
     }
-    else{
+    else
+    {
         s->top++;
-        s->items[s->top]=newitem;
+        s->items[s->top] = newitem;
     }
     count++;
 }
 
 // Remove element from stack
-void pop(mystack *s){
-    if(isEmpty){
+void pop(mystack *s)
+{
+    if (isEmpty(s))
+    {
         printf("Stack is Empty\n");
     }
-    else{
-        printf("Item popped=%d",s->items[s->top]);
+    else
+    {
+        printf("Item popped=%d", s->items[s->top]);
         s->top--;
     }
     count--;
@@ -58,21 +68,24 @@ void pop(mystack *s){
 }
 
 // diplaying element of the stack
-void printStack(mystack *s){
+void printStack(mystack *s)
+{
     printf("stack :");
-    for(int i=0;i<count;i++){
-        printf("%d",s->items[i]);
+    for (int i = s->top; i >= 0; i--)
+    {
+        printf("index: %d --> value: %d\t", i, s->items[i]);
     }
     printf("\n");
 }
 
 // main method
-int main(){
-    mystack *s=(mystack*)malloc(sizeof(mystack));
+int main()
+{
+    mystack *s = (mystack *)malloc(sizeof(mystack));
     createEmptyStack(s);
-    push(s,11);
-    push(s,2);
-    push(s,10);
+    push(s, 11);
+    push(s, 2);
+    push(s, 10);
 
     printStack(s);
 
